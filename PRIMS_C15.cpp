b06ec1691fd1f1dd0eeb2906da086ff83f351197
Aryan Chogale <128447275+Aryan07CH@users.noreply.github.com>
2023-05-10 08:41:23 +0530

    Update PRIMS_C15.cpp

--- PRIMS_C15.cpp
+++ PRIMS_C15.cpp
@@ -1,3 +1,11 @@
+/*
+PROBLEM STATEMENT:
+	You have a business with several offices; you want to lease phone lines to connect them up
+	with each other; and the phone company charges different amounts of money to connect
+	different pairs of cities. You want a set of lines that connects all your offices with minimum total cost.
+	Solve the problem by suggesting appropriate data structures.
+*/
+
 #include<iostream>
 using namespace std;
 
